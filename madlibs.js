7af60e61be778acff96ab0dddfd7c76ba4eaba18
Wren Purcell <wpurcell@theknowledgehouse.org>
2024-08-07 15:07:06 +0000

    added madlibs example

---
+++ madlibs.js
@@ -0,0 +1,21 @@
+//variables - analogy - moving box - labeled on the outside, stores stuff inside 
+
+// let keyword decalres a variable that can have its value changed
+// const - value cannot be changed 
+
+//data type for words? string 
+let noun1 = "dog"
+let adjective1 = "quick"
+//add a variable that holds a number
+
+//add a variable that holds a number created by an expression 
+
+//add a variable that holds a true or false value 
+
+//add a prompt and save it to a variable 
+prompt("enter a verb")
+
+//concatenate our variables together into one string 
+let output = "The " + noun1 + " jumped over the " + adjective1
+
+console.log(output)
